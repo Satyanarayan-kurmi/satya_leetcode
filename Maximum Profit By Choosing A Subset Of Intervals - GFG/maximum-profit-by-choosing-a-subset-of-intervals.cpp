6b10eb32da26f71353ba693440e5ff49d6c86748
Satyanarayan-kurmi <95236961+Satyanarayan-kurmi@users.noreply.github.com>
2023-01-04 10:51:33 +0530

    Added solution - LeetHub

--- Maximum Profit By Choosing A Subset Of Intervals - GFG/maximum-profit-by-choosing-a-subset-of-intervals.cpp
+++ Maximum Profit By Choosing A Subset Of Intervals - GFG/maximum-profit-by-choosing-a-subset-of-intervals.cpp
@@ -9,39 +9,40 @@ using namespace std;
 class Solution {
   public:
     vector<vector<int>> vec;
-    int find_index(int element,int id){
-        int l=0,h=id-1;
-        int ans=-1;
-        while(l<=h){
-            int mid=(l+h)/2;
-            if(vec[mid][0]<=element){
-                ans=mid;
-                l=mid+1;
-            }
-            else h=mid-1;
+    // int find_index(int element,int id){
+    //     int l=0,h=id-1;
+    //     int ans=-1;
+    //     while(l<=h){
+    //         int mid=(l+h)/2;
+    //         if(vec[mid][0]<=element){
+    //             ans=mid;
+    //             l=mid+1;
+    //         }
+    //         else h=mid-1;
             
-        }
-        return ans;
+    //     }
+    //     return ans;
         
-    }
-    static bool comp(vector<int>& a,vector<int>& b){
-        return a[0]<b[0];
-    }
+    // }
     int maximum_profit(int n, vector<vector<int>> &intervals) {
         vector<int> dp(n);
         for(int i=0;i<n;i++){
             vec.push_back({intervals[i][1],intervals[i][0],intervals[i][2]});
         }
-        sort(vec.begin(),vec.end(),comp);
+        sort(vec.begin(),vec.end());
+        vector<int> arr;
         for(int i=0;i<n;i++){
             dp[i]=vec[i][2];
             int ele=vec[i][1];
-            int index=find_index(ele,i);
-            if(index!=-1)
+            // int index=find_index(ele,i);
+            int index=upper_bound(arr.begin(),arr.end(),ele)-arr.begin();
+            index--;
+            if(index>-1)
                 dp[i]+=dp[index];
             if(i>0)
                 dp[i]=max(dp[i],dp[i-1]);
             // cout<<dp[i]<<" ";
+            arr.push_back(vec[i][0]);
             
         }
         
